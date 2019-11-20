e92939d9027322ef26fe41333aadff35d248c34f
xatu <xatu@xatu.local>
2019-11-20 13:51:06 +0900

    add const

--- nntp.js
+++ nntp.js
@@ -3,20 +3,22 @@ const fs = require("fs")
 const ws = require('ws')
 const execSync = require('child_process').execSync
 
-const NHOST = "localhost"//"nh1.u-aizu.ac.jp"//
-const NPORT = 9119//119//
+const NHOST = "localhost" //"nh1.u-aizu.ac.jp"//
+const NPORT = 9119 //119//
 const PORT = 8119
-server = new ws.Server({port: PORT})
+const server = new ws.Server({
+  port: PORT
+})
 
-server.on("connection", wsock=>{
+server.on("connection", wsock => {
   const nntpConnection = net.connect(NPORT, NHOST).setKeepAlive(true)
-  nntpConnection.on("data", d=>{
+  nntpConnection.on("data", d => {
     fs.writeFileSync("tmp", d)
     wsock.send(execSync("nkf -w tmp").toString())
   })
-  nntpConnection.on("close", ()=>console.log("NNTPConnection closed."))
-  nntpConnection.on("error", (a)=>console.log("error in NNTPConnection", a))
-  wsock.on("message", msg=>{
+  nntpConnection.on("close", () => console.log("NNTPConnection closed."))
+  nntpConnection.on("error", a => console.log("error in NNTPConnection", a))
+  wsock.on("message", msg => {
     console.log(msg)
     nntpConnection.write(msg)
   })
--- viewer.js
+++ viewer.js
@@ -3,8 +3,10 @@ sock.addEventListener("open", e =>
   console.log("WebSocketに接続しました")
 )
 window.addEventListener("load", () => {
-  sock.addEventListener("message", handleMessage, {once: true})
-  document.getElementById("show_list").addEventListener("click", e =>{
+  sock.addEventListener("message", handleMessage, {
+    once: true
+  })
+  document.getElementById("show_list").addEventListener("click", e => {
     sendMessage("LIST\n")
   })
 })
@@ -16,11 +18,13 @@ const hasMessageEnd = msg => /\r\n\.\r\n/.test(msg)
 const updateList = str => {
   const list = document.getElementById("groups")
   list.innerHTML = ""
-  for(let line of str.split("\r\n.\r\n")[0].replace(/^[1-9].+\r\n/mg, "").split("\r\n")) {
+  for (let line of str.split("\r\n.\r\n")[0].replace(/^[1-9].+\r\n/mg, "").split("\r\n")) {
     list.innerHTML += `<li onclick="sendMessage('GROUP ' + this.innerHTML.split(' ')[0]+'\\n')">${line.split(" ")[0]}</li>`
   }
 }
-const setHandler = () => sock.addEventListener("message", handleMessage, {once: true})
+const setHandler = () => sock.addEventListener("message", handleMessage, {
+  once: true
+})
 const handleMessage = msg => {
   const responseCode = getResponseCode(msg.data)
   switch (responseCode) {
@@ -39,22 +43,26 @@ const handleMessage = msg => {
       document.getElementById("article").innerHTML = ""
       const articleList = document.getElementById("article_list")
       articleList.innerHTML = ""
-      if(Number(msg.data.split(" ")[2]) > Number(msg.data.split(" ")[3])) console.log(msg.data.split(" ")[4] + "there is no article")
+      if (Number(msg.data.split(" ")[2]) > Number(msg.data.split(" ")[3])) console.log(msg.data.split(" ")[4] + "there is no article")
       else {
-        console.log(msg.data.split(" ")[4] + msg.data.split(" ")[2] +" to "+ msg.data.split(" ")[3])
-        for(let i = Number(msg.data.split(" ")[2]);i <= Number(msg.data.split(" ")[3]); i++)
+        console.log(msg.data.split(" ")[4] + msg.data.split(" ")[2] + " to " + msg.data.split(" ")[3])
+        for (let i = Number(msg.data.split(" ")[2]); i <= Number(msg.data.split(" ")[3]); i++)
           articleList.innerHTML += "<span onclick='sendMessage(`ARTICLE `+`" + i + "\n`)'>" + i + " </span>"
       }
       setHandler()
       break
     case "215":
       console.log("list received")
-      if(!hasMessageEnd(msg.data)) sock.addEventListener("message", nextMessage(msg.data, listReceived), {once: true})
+      if (!hasMessageEnd(msg.data)) sock.addEventListener("message", nextMessage(msg.data, listReceived), {
+        once: true
+      })
       else listReceived(msg.data)
       break
     case "220":
       console.log("article received")
-      if(!hasMessageEnd(msg.data)) sock.addEventListener("message", nextMessage(msg.data, articleReceived), {once: true})
+      if (!hasMessageEnd(msg.data)) sock.addEventListener("message", nextMessage(msg.data, articleReceived), {
+        once: true
+      })
       else articleReceived(msg.data)
       break
     default:
@@ -62,10 +70,12 @@ const handleMessage = msg => {
       setHandler()
   }
 }
-const nextMessage = (prevMsg, callback) => (msg) => {
+const nextMessage = (prevMsg, callback) => msg => {
   msg = prevMsg + msg.data
   console.log(callback)
-  if(!hasMessageEnd(msg)) sock.addEventListener("message", nextMessage(msg, callback), {once: true})
+  if (!hasMessageEnd(msg)) sock.addEventListener("message", nextMessage(msg, callback), {
+    once: true
+  })
   else callback(msg)
 }
 const listReceived = msg => {
